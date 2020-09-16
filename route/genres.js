4cbeb82625f2e4dfd6496019ff18b2531ab8142e
gowthamBhat <gowthambhat793@gmail.com>
2020-09-16 18:53:20 +0530

    some imp comments are added about exception handling

--- route/genres.js
+++ route/genres.js
@@ -3,6 +3,7 @@ router = express.Router();
 const auth = require('../middleware/middlewareAuth');
 const admin = require('../middleware/middlewareAdmin');
 const asyncMiddleware = require('../middleware/asyncMiddleware');
+const winston = require('winston');
 
 
 const mongoose = require('mongoose');
@@ -22,8 +23,8 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/:id', asyncMiddleware(async (req, res) => {  //asyncMiddleware is added to dynamically gen try catch block
-    const gen = await Genres.findById(req.params.id);
-    res.send(gen);
+    const gen = await Genres.findById(req.params.id);         //for only this route async middleware is added bcz i already wrote error handling code for all other routes
+    res.send(gen);                                              //and changing them now is hectic, so im letting it be
 }));
 
 
@@ -48,8 +49,9 @@ router.post('/', auth, async (req, res) => {    //*in routers the second paramet
 
     }
     catch (err) {
-        res.status(400).send("Due to some internal issue POST method cant be performed");
+        res.status(500).send("Due to some internal issue POST method cant be performed");
         console.log(err);
+        winston.error(err.message, err);
     }
 });
 
@@ -60,12 +62,15 @@ router.put('/:id', auth, async (req, res) => {
 
     try {
         const val = validate(req.body);
-        if (val.error) return res.status(400).send(val.error.details[0].message);
+        if (val.error) return res.status(400).send(val.error.details[0].message)
+
 
         // const genFind = await Genres.findById(req.params.id);
         // if (!genFind) return res.status(404).send('data not found');
 
-
+        //! here it's best to use promises rather using async functions,bcz i coudn't print specific error methods here
+        //! it's throwing an exception whenever it's not found specific data in database, so conditonal statement doesn't work properly
+        //! so i coudn't print specific messages to that exceptions
         const gen = await Genres.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
 
         // const gen = await Genres.updateOne({ _id: req.params.id }, { name: req.body.name });
@@ -76,6 +81,7 @@ router.put('/:id', auth, async (req, res) => {
     catch (ex) {
         res.status(404).send('data not found');
         console.log(ex);
+        winston.error(ex.message, ex);
     }
 
 });
