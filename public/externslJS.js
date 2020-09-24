585762a3738256142a3411b64713d91ada8fdc85
gowthamBhat <gowthambhat793@gmail.com>
2020-09-25 00:36:29 +0530

    minor design change

---
+++ public/externslJS.js
@@ -0,0 +1,9 @@
+// var text = "m o v i e P o i n t";
+// text = text.split(" ");
+// var colors = ["red", "green", "rgb(0, 162, 232)"];
+// var len = text.length;
+// var j = text.join('');
+// console.log(j);
+// for (var i = 0; i < text.length; i++) {
+//     document.write('<span style = "color: ' + colors[i % 3] + '">' + (j[i]).join('') + ' </span>');
+// }
